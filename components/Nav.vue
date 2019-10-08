77b0f1ca16acf2df008f4ddce5b7b4d0c447c66d
Henry Desroches <yo@henry.codes>
2019-10-07 22:49:48 -0600

    💻  - Remove unnecessary class logic in nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,10 +2,7 @@
   <nav class="nav">
     <ul class="nav__list">
       <li class="nav__list-item">
-        <nuxt-link
-          to="/"
-          :class="isHome ? 'nav__list-link nav__list-link--active' : 'nav__list-link'"
-        >home</nuxt-link>
+        <nuxt-link to="/" class="nav__list-link">home</nuxt-link>
       </li>
       <li class="nav__list-item">
         <nuxt-link to="/#blog" class="nav__list-link">blog</nuxt-link>
